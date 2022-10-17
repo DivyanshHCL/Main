e3f5dcaf5927f74010326e2c9a14b152067c4c49
DivyanshHCL <113426234+DivyanshHCL@users.noreply.github.com>
2022-10-17 18:49:00 +0530

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,12 @@
+package com.hcl.practice.mathutilities;
+
+public class EvenOrOdd {
+
+	public static void evenOrOdd(int number) {
+			if (number % 2 == 0) {
+				System.out.println("even");
+			} else
+				System.out.println("odd");
+
+	}
+}	
